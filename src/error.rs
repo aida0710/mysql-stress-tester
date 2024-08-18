978c2e688b2fdc7a6cf1bd2ee1799ed9f8e9a212
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-08-19 08:15:29 +0900

    mysqlに接続できるように

--- src/error.rs
+++ src/error.rs
@@ -1,19 +1,21 @@
+use std::fmt;
+use mysql::Error as MySqlError;
+
 #[derive(Debug)]
 pub enum AppError {
     IoError(std::io::Error),
     JsonError(json::Error),
     MissingField(String),
-    InvalidType(String, String),
+    MySqlError(MySqlError),
 }
 
-impl std::fmt::Display for AppError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+impl fmt::Display for AppError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             AppError::IoError(e) => write!(f, "IO error: {}", e),
             AppError::JsonError(e) => write!(f, "JSON parsing error: {}", e),
             AppError::MissingField(field) => write!(f, "Missing field in JSON: {}", field),
-            AppError::InvalidType(field, expected_type) =>
-                write!(f, "Invalid type for field '{}', expected {}", field, expected_type),
+            AppError::MySqlError(field) => write!(f, "sqlの接続時にエラーが発生しました: {}", field)
         }
     }
 }
@@ -29,5 +31,11 @@ impl From<std::io::Error> for AppError {
 impl From<json::Error> for AppError {
     fn from(err: json::Error) -> AppError {
         AppError::JsonError(err)
+    }
+}
+
+impl From<MySqlError> for AppError {
+    fn from(err: MySqlError) -> AppError {
+        AppError::MySqlError(err)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 mod config;
 mod error;
 
+use mysql::{Pool, prelude::*};
 use crate::config::Config;
 use crate::error::AppError;
 
 fn main() -> Result<(), AppError> {
     const PREFIX: &str = "[起動処理]";
-
-    println!("{}: 設定ファイルを読み込んでいます。", PREFIX);
-
+    println!("{}: 設定ファイルを読み込んでいます", PREFIX);
     let config: Vec<String> = Config::from_file("./config/mysql.json", vec!["port", "username"])?;
     println!("{}: 設定ファイルの内容: {:?}", PREFIX, config);
+    println!("{}: 設定ファイルの読み込みが完了しました", PREFIX);
+
+    let url = "mysql://root:password@localhost:3306/db_name";
+    let pool = Pool::new(url)?;
+    let mut connect = pool.get_conn()?;
+
+    connect.query_drop( r"create database test")?;
 
     Ok(())
 }
