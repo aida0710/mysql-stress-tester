b50e8ad2c4c4c08fb0bcea8a0989c658f656c645
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-08-21 18:54:25 +0900

    自由にクエリが送信できるように

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,57 @@
 mod config;
 mod error;
 
-use mysql::{Pool, prelude::*};
 use crate::config::Config;
 use crate::error::AppError;
+use mysql::{prelude::*, Pool};
 
 fn main() -> Result<(), AppError> {
     const PREFIX: &str = "[起動処理]";
     println!("{}: 設定ファイルを読み込んでいます", PREFIX);
-    let config: Vec<String> = Config::from_file("./config/mysql.json", vec!["port", "username"])?;
+    let config: Vec<String> = Config::from_file("./config/mysql.json", vec![
+        "address",
+        "port",
+        "username",
+        "password",
+    ])?;
     println!("{}: 設定ファイルの内容: {:?}", PREFIX, config);
     println!("{}: 設定ファイルの読み込みが完了しました", PREFIX);
 
-    let url = "mysql://root:password@localhost:3306/db_name";
-    let pool = Pool::new(url)?;
-    let mut connect = pool.get_conn()?;
+    let url = format!(
+        "mysql://{username}:{password}@{address}:{port}",
+        username = config[2],
+        password = config[3],
+        address = config[0],
+        port = config[1]
+    );
+    let pool = Pool::new(url.as_str())?;
+    let mut connect = pool.get_conn().expect("接続に失敗しました");
 
-    connect.query_drop( r"create database test")?;
+    loop {
+        let mut input = String::new();
+        println!("SQLクエリを入力してください:");
+        std::io::stdin().read_line(&mut input).expect("標準入力に失敗しました");
+        if input.trim().is_empty() {
+            println!("空文字列は無効です");
+            continue;
+        }
 
+        if input.trim().to_lowercase() == "exit" {
+            println!("プログラムを終了します");
+            break;
+        }
+
+        match connect.query_iter(input.trim()) {
+            Ok(result) => {
+                for row in result {
+                    match row {
+                        Ok(row) => println!("{:?}", row),
+                        Err(e) => println!("行の取得中にエラーが発生しました: {}", e),
+                    }
+                }
+            }
+            Err(e) => println!("クエリの実行に失敗しました: {}", e),
+        }
+    }
     Ok(())
 }
