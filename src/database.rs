df9ac05c0d50fe6d4575ddc14ef7f5b705730535
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-10-16 20:23:16 +0900

    コメントを追加

--- src/database.rs
+++ src/database.rs
@@ -5,6 +5,7 @@ pub fn create_pool(url: &str) -> Result<Pool, Error> {
     Pool::new(url)
 }
 
+// 起動時に一度だけ実行するクエリ
 pub fn single_execute_query<T: Queryable>(conn: &mut T) -> Result<(), Error> {
     conn.exec_drop("DROP TABLE IF EXISTS load_test_table", ())?;
 
@@ -20,15 +21,13 @@ pub fn single_execute_query<T: Queryable>(conn: &mut T) -> Result<(), Error> {
     Ok(())
 }
 
+// 繰り返し実行されるクエリ
 pub fn execute_query<T: Queryable>(conn: &mut T) -> Result<(), Error> {
-    // ここにカスタムクエリを直接記述します
-    // 例: SELECT クエリ
     // let _: Vec<Row> = conn.query("SELECT * FROM load_test_table LIMIT 10")?;
 
-    // 例: INSERT クエリ
     conn.exec_drop(
         "INSERT INTO load_test_table (column1, column2) VALUES (?, ?)",
-        ("value1", 42),
+        ("value1", 30),
     )?;
 
     Ok(())
